5d2f2736bd6abbe5b8c52df9df039ada2cedc9e5
Lucas Dainez <lucdainez@gmail.com>
2022-12-14 19:45:06 -0300

    feat(app.ts): adicionar rotas de usuário

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import productsRoute from './routes/products.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productsRoute);
+
+app.use('/users', usersRoute);
 
 export default app;
