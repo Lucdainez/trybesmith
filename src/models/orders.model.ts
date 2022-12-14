d5f32a2daed6be2d54ac24a4ffb0ff13694b8059
Lucas Dainez <lucdainez@gmail.com>
2022-12-14 20:50:08 -0300

    feat(orders.model.ts): adicionar chamada ao banco de dados

---
+++ src/models/orders.model.ts
@@ -0,0 +1,22 @@
+import { RowDataPacket } from 'mysql2/promise';
+import connection from './connection';
+import { Orders } from '../types';
+
+const getAllOrders = async (): Promise<Orders[]> => {
+  const query = `SELECT 
+  o.id,
+  o.user_id AS userId,
+  JSON_ARRAYAGG(p.id) AS productsIds
+FROM
+  Trybesmith.orders AS o
+      INNER JOIN
+  Trybesmith.products AS p
+WHERE
+  o.id = p.order_id GROUP BY o.id;`;
+  const [result] = await connection.execute<RowDataPacket[] & Orders[]>(query);
+  return result;
+};
+
+export default {
+  getAllOrders,
+};
