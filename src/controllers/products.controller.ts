2765429279f72f08eee3a1da06cf503b8f87fd5e
Lucas Dainez <lucdainez@gmail.com>
2022-12-14 18:51:21 -0300

    feat(products.controller.ts): adicionar requisição para trazer todos os produtos

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,16 +1,20 @@
 import { Request, Response } from 'express';
 import productsService from '../services/products.service';
-import { ReturnProduct } from '../types';
+import { ReturnProduct, AllProducts } from '../types';
 
 const postProducts = async (req: Request, res: Response) => {
   const { name, amount } = req.body;
   const productId: number = await productsService.postProducts({ name, amount });
-  const result: ReturnProduct = {
-    id: productId,
-    name,
-    amount,
-  };
+  const result: ReturnProduct = { id: productId, name, amount };
   return res.status(201).json(result);
 };
 
-export default { postProducts };
+const getAllProducts = async (_req: Request, res: Response) => {
+  const result: AllProducts = await productsService.getAllProducts();
+  return res.status(200).json(result);
+};
+
+export default {
+  postProducts,
+  getAllProducts,
+};
