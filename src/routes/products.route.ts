91eff20bdd590e9515e9fb22fce32b5ad8ab211a
Lucas Dainez <lucdainez@gmail.com>
2022-12-15 12:48:22 -0300

    feat(products.route.ts): adicionar validação a rota

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controller';
+import productValidationMiddleware from '../middlewares/productValidation.middleware';
 
 const router = Router();
 
-router.post('/', productsController.postProducts);
+router.post(
+  '/',
+  productValidationMiddleware.nameValidationObrigatory,
+  productValidationMiddleware.amountValidationObrigatory,
+  productValidationMiddleware.amountLengthValidation,
+  productValidationMiddleware.nameToStringValidation,
+  productValidationMiddleware.amountToStringValidation,
+  productValidationMiddleware.nameLengthValidation,
+  productsController.postProducts,
+);
 
 router.get('/', productsController.getAllProducts);
 
