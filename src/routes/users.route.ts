d366f5402141c2a02c06299f54a9d8a45527e867
Lucas Dainez <lucdainez@gmail.com>
2022-12-15 15:55:22 -0300

    feat(users.route.ts): adicionar validações a rota

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,8 +1,24 @@
 import { Router } from 'express';
 import usersController from '../controllers/users.controller';
+import userValidationMiddleware from '../middlewares/userValidation.middleware';
 
 const router = Router();
 
-router.post('/', usersController.postUser);
+router.post(
+  '/',
+  userValidationMiddleware.usernameIsRequireValidation,
+  userValidationMiddleware.passwordIsRequireValidation,
+  userValidationMiddleware.usernameToStringValidation,
+  userValidationMiddleware.usernameLengthValidation,
+  userValidationMiddleware.levelLengthValidation,
+  userValidationMiddleware.passwordLengthValidation,
+  userValidationMiddleware.vocationIsRequireValidation,
+  userValidationMiddleware.vocationToStringValidation,
+  userValidationMiddleware.passwordToStringValidation,
+  userValidationMiddleware.vocationLengthValidation,
+  userValidationMiddleware.levelIsRequireValidation,
+  userValidationMiddleware.levelNumberValidation,
+  usersController.postUser,
+);
 
 export default router;
