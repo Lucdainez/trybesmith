7841731575b4a94dd4cbf46e2fef183dd60b4d36
Lucas Dainez <lucdainez@gmail.com>
2022-12-16 18:11:38 -0300

    feat(orders.controller.ts): adicionar requisição para novo pedido

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import ordersService from '../services/orders.service';
+
 import { Orders } from '../types';
 
 const getAllOrders = async (_req: Request, res: Response) => {
@@ -7,6 +8,14 @@ const getAllOrders = async (_req: Request, res: Response) => {
   return res.status(200).json(result);
 };
 
+const postNewOrder = async (req: Request, res: Response) => { 
+  const { productsIds } = req.body;
+  const { authorization } = req.headers;
+  const result = await ordersService.postNewOrder(productsIds, authorization as string);
+  return res.status(201).json(result);
+};
+
 export default {
   getAllOrders,
+  postNewOrder,
 };
