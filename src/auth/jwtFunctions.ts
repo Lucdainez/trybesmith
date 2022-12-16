db134ca23b7269f57554a7822da656c6e59d0be9
Lucas Dainez <lucdainez@gmail.com>
2022-12-16 18:10:33 -0300

    feat(jwtFunctions.ts): adicionar função que verifica token

--- src/auth/jwtFunctions.ts
+++ src/auth/jwtFunctions.ts
@@ -1,4 +1,5 @@
-import jwt from 'jsonwebtoken';
+import { verify, sign } from 'jsonwebtoken';
+import { Token } from '../types';
 // import { JwtConfig } from '../types';
 
 const secret: string = process.env.JWT_SECRET || 'seuSecretAqui';
@@ -8,12 +9,29 @@ const secret: string = process.env.JWT_SECRET || 'seuSecretAqui';
 //   expiresIn: '1d',
 // };
 
-const createToken = (userId: number, username: string): string => jwt
-  .sign({ data: userId, username }, secret, {
-    algorithm: 'HS256',
-    expiresIn: '1d',
-  });
+const createToken = (userId: number, username: string): string => sign({
+  id: userId, username,
+}, secret, {
+  algorithm: 'HS256',
+  expiresIn: '1d',
+});
+
+const verifyToken = (authorization: string): Token => {
+  try {
+    const payload = verify(authorization, secret);
+    return payload as Token;  
+  } catch (error) {
+    const payload: Token = {
+      id: 0,
+      username: '',
+      iat: 0,
+      exp: 0,
+    };
+    return payload;
+  }  
+};
 
 export default {
   createToken,
+  verifyToken,
 };
