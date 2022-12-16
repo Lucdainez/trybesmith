ecfef726f64bd305878fd317cdcf5c040903f1ff
Lucas Dainez <lucdainez@gmail.com>
2022-12-16 18:21:24 -0300

    feat(orders.route.ts): adicionar validação a rota

--- src/routes/orders.route.ts
+++ src/routes/orders.route.ts
@@ -1,8 +1,20 @@
 import { Router } from 'express';
 import ordersController from '../controllers/orders.controller';
+import productValidationMiddleware from '../middlewares/productValidation.middleware';
+import tokenValidationMiddleware from '../middlewares/tokenValidation.middleware';
 
 const router = Router();
 
 router.get('/', ordersController.getAllOrders);
+
+router.post(
+  '/',
+  tokenValidationMiddleware.tokenNotFound,
+  tokenValidationMiddleware.invalidToken,
+  productValidationMiddleware.productsIdsIsRequiredValidation,
+  productValidationMiddleware.productsIdsArrayValidation,
+  productValidationMiddleware.productsIdsNumberValidation,
+  ordersController.postNewOrder,
+);
 
 export default router;
