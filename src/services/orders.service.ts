b6ed57257db21f1647f28a5ff3deb7f3572c37ff
Lucas Dainez <lucdainez@gmail.com>
2022-12-16 18:22:36 -0300

    feat(order.service.ts): adicionar regra de negócio a camada service

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,11 +1,19 @@
 import ordersModel from '../models/orders.model';
 import { Orders } from '../types';
+import jwtFunctions from '../auth/jwtFunctions';
 
 const getAllOrders = async (): Promise<Orders[]> => {
   const allProducts: Orders[] = await ordersModel.getAllOrders();
   return allProducts;
 };
 
+const postNewOrder = async (productsIds: number[], authorization: string) => { 
+  const payload = jwtFunctions.verifyToken(authorization);
+  await ordersModel.postNewOrder(productsIds, payload.id);
+  return { userId: payload.id, productsIds };
+};
+
 export default {
   getAllOrders,
+  postNewOrder,
 };
